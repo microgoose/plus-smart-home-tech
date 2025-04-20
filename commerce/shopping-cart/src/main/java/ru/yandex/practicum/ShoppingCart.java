fb503ece158284675f2493987afb55e472985be5
microgoose <gusakmachine@yandex.ru>
2025-04-20 13:58:00 +0300

    feat: инициализированы новые commerce-сервисы, всем микросервисами установлен случайный порт

---
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCart.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum;
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/Warehouse.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum;
+
+public class Warehouse {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
